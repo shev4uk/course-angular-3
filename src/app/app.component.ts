e0b8f38bfdb30891b4f52e11f5b039998860d641
yshevc <yshevc@softserveinc.com>
2020-09-21 20:57:21 +0300

    lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = 'Lesson';
+  numberLesson = 1;
+  tacks = ['intro', 'start project', 'build', 'homework'];
+  posts = [{
+    title: 'post 1',
+    text: 'test post 1'
+  }, {
+    title: 'post 2',
+    text: 'test post 2'
+  }, {
+    title: 'post 3',
+    text: 'test post 3'
+  }]
+  state = false;
+
+  dropdown() {
+    this.state = !this.state;
+  }
 }
